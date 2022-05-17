b47d2e6027b14b30506d1bcadd586cdc5a293802
Valentine Samoylov <valentine.samoylov@gmail.com>
2022-05-17 17:16:30 +0300

    [Context] redudant declarations have been removed from TimerContext.

--- src/context/TimerContext/index.tsx
+++ src/context/TimerContext/index.tsx
@@ -6,12 +6,6 @@ interface ITimerContext {
   setIsRunning: (fn: boolean) => void
   counter: number
   setCounter: (fn: any) => void
-  second: string | number
-  setSecond: (fn: string | number) => void
-  minute: string | number
-  setMinute: (fn: string | number) => void
-  hour: string | number
-  setHour: (fn: string | number) => void
 }
 
 export const TimerContext = createContext({} as ITimerContext)
@@ -19,21 +13,12 @@ export const TimerContext = createContext({} as ITimerContext)
 const TimerProvider = ({ children }: { children: ReactNode }) => {
   const [isRunning, setIsRunning] = useState<boolean>(false)
   const [counter, setCounter] = useState<number>(0)
-  const [second, setSecond] = useState<string | number>('00')
-  const [minute, setMinute] = useState<string | number>('00')
-  const [hour, setHour] = useState<string | number>('00')
 
   const timerValue = {
     isRunning,
     setIsRunning,
     counter,
     setCounter,
-    second,
-    setSecond,
-    minute,
-    setMinute,
-    hour,
-    setHour,
   }
 
   return (
